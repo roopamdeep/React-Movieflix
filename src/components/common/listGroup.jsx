badeab7de6b6aa4c55b30d02af075f29015c93e5
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2020-10-23 23:28:41 -0400

    added list group component for filtering list of movies

---
+++ src/components/common/listGroup.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+const ListGroup = (props) => {
+    const {items, onItemSelect, textProperty, valueProperty,selectedItem} = props
+
+
+    return (  
+        <ul className="list-group">
+            {items.map(item  => <li key={item[valueProperty]} className={item === selectedItem ? "list-group-item active" :"list-group-item" } onClick={()=> onItemSelect(item)}>{item[textProperty]}</li>)}
+           
+        </ul>
+    );
+}
+ListGroup.defaultProps = {
+    textProperty:"name",
+    valueProperty:"_id"
+}
+ 
+export default ListGroup;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,8 +3,14 @@ import { getMovies } from "../services/fakeMovieService";
 import Like from "../components/common/like";
 import Pagination from "../components/common/pagination";
 import { paginate } from "../utils/paginate";
+import ListGroup from '../components/common/listGroup'
+import { getGenres } from "../services/fakeGenreService";
 class Movies extends Component {
-  state = { movies: getMovies(), pageSize: 4, currentPage: 1 };
+  state = { movies: [], pageSize: 4, currentPage: 1 ,genres:[]};
+  componentDidMount(){
+    const genres = [{name:'All Genres'},...getGenres()]
+    this.setState({movies:getMovies(),genres})
+   }
   handleDelete = (movie) => {
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies: movies });
@@ -20,17 +26,30 @@ class Movies extends Component {
   handlePageChange = (page) => {
     this.setState({ currentPage: page });
   };
+  handleGenreSelect = genre =>{
+    this.setState({selectedGenre:genre, currentPage:1})
+
+  }
+  
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const { pageSize, currentPage, movies: allMovies,selectedGenre } = this.state;
     if (count === 0) return <p>There are no movies in the database</p>;
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const filtered = selectedGenre && selectedGenre._id ? allMovies.filter(m => m.genre._id === selectedGenre._id) : allMovies;
+    const movies = paginate(filtered, currentPage, pageSize);
     // console.log(allMovies);
     // console.log(movies);
 
     return (
-      <React.Fragment>
-        <p>Showing {count} movies in the database</p>
+      <div className='row'>
+        <div className='col-3'>
+          <ListGroup 
+          items={this.state.genres}  
+          selectedItem={this.state.selectedGenre}
+           onItemSelect={this.handleGenreSelect}/>
+          </div>
+          <div className='col'>
+          <p>Showing {filtered.length} movies in the database</p>
         <table className="table">
           <thead>
             <tr>
@@ -68,12 +87,15 @@ class Movies extends Component {
           </tbody>
         </table>
         <Pagination
-          itemsCount={count}
+          itemsCount={filtered.length}
           pageSize={pageSize}
           onPageChange={this.handlePageChange}
           currentPage={currentPage}
         />
-      </React.Fragment>
+          
+        </div>
+       
+      </div>
     );
   }
 }
